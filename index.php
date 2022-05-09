149d6e4773a3ae9d96032db01b224c4c48f6ba18
mnafis5 <94342046+mnafis5@users.noreply.github.com>
2022-05-09 09:10:31 +0700

    index.php about views

---
+++ index.php
@@ -0,0 +1,80 @@
+<div class="card">
+    <div class="container">
+        <div class="about">
+            <div class="paragraf">
+            <div class="col-md-12 p-2">
+				<div style="text-align:center; text-indent:35pt"><span style="color:#2980b9"><span style="font-size:12pt"><strong><span style="font-size:20pt"><span style="font-family:Tahoma">TENTANG</span></span></strong></span></span><span style="color:#16a085"><span style="font-size:12pt"><strong><span style="font-size:20pt"><span style="font-family:Tahoma"> </span></span></strong></span></span><span style="color:#c0392b"><span style="font-size:12pt"><strong><span style="font-size:20pt"><span style="font-family:Tahoma">L</span></span></strong></span></span><span style="color:#f1c40f"><span style="font-size:12pt"><strong><span style="font-size:20pt"><span style="font-family:Tahoma">P</span></span></strong></span></span><span style="color:#27ae60"><span style="font-size:12pt"><strong><span style="font-size:20pt"><span style="font-family:Tahoma">K</span></span></strong></span></span><span style="color:#16a085"><span style="font-size:12pt"><strong><span style="font-size:20pt"><span style="font-family:Tahoma"> </span></span></strong></span></span><span style="color:#3498db"><span style="font-size:12pt"><strong><span style="font-size:20pt"><span style="font-family:Tahoma">PEMBANGUNAN&nbsp;</span></span></strong></span></span></div>
+
+                    <div style="text-align:center; text-indent:35pt">&nbsp;</div>
+
+                <p style="text-align:justify"><span style="color:#c0392b"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma; padding-left: 60px;">L</span></span></strong></span></span><span style="color:#f1c40f"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">P</span></span></strong></span></span><span style="color:#27ae60"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">K</span></span></strong></span></span><span style="color:#16a085"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma"> </span></span></strong></span></span><span style="color:#3498db"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">PEMBANGUNAN&nbsp;</span></span></strong></span></span><span style="font-size:12pt"><span style="font-family:Arial">, merupakan salah satu Lembaga Pendidikan Komputer di Kota Pekalongan, berdiri pada tahun 2000. Beralamatkan di Jl. Tentara Pelajar No.15 Pekalongan Telp. (0285) 412600. </span></span></p>
+
+<p style="text-align:justify; padding-top: 10px;"><span style="color:#c0392b"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma; padding-left: 60px;">L</span></span></strong></span></span><span style="color:#f1c40f"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">P</span></span></strong></span></span><span style="color:#27ae60"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">K</span></span></strong></span></span><span style="color:#16a085"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma"> </span></span></strong></span></span><span style="color:#3498db"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">PEMBANGUNAN&nbsp;</span></span></strong></span></span><span style="font-size:12pt"><span style="font-family:Arial"> sudah banyak kemajuan dan perkembangan dalam kurun waktu perjalanannya, </span></span><span style="color:#c0392b"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">L</span></span></strong></span></span><span style="color:#f1c40f"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">P</span></span></strong></span></span><span style="color:#27ae60"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">K</span></span></strong></span></span><span style="color:#16a085"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma"> </span></span></strong></span></span><span style="color:#3498db"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">PEMBANGUNAN</span></span></strong></span></span><span style="font-size:12pt"><span style="font-family:Arial"> menghadirkan sebuah pendidikan non formal bagi semua kalangan dan&nbsp;telah banyak meluluskan siswanya</span></span></p>
+
+<p style="text-align:justify; padding-top:10px;"><span style="font-size:12pt; padding-left:60px;"><span style="font-family:Arial">Lulusan dari </span></span><span style="color:#c0392b"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">L</span></span></strong></span></span><span style="color:#f1c40f"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">P</span></span></strong></span></span><span style="color:#27ae60"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">K</span></span></strong></span></span><span style="color:#16a085"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma"> </span></span></strong></span></span><span style="color:#3498db"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">PEMBANGUNAN&nbsp;</span></span></strong></span></span><span style="font-size:12pt"><span style="font-family:Arial"> sudah banyak yang bekerja baik di Instansi Pemerintah dan Swasta, ada juga yang berwiraswata.</span></span></p>
+
+<p style="text-align:justify; padding-top:10px;"><span style="font-size:12pt; padding-left:60px; "><span style="font-size:11.0pt"><span style="font-family:Arial">Dalam Programnya </span></span></span><span style="color:#c0392b"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">L</span></span></strong></span></span><span style="color:#f1c40f"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">P</span></span></strong></span></span><span style="color:#27ae60"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">K</span></span></strong></span></span><span style="color:#16a085"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma"> </span></span></strong></span></span><span style="color:#3498db"><span style="font-size:12pt"><strong><span style="font-size:11.0pt"><span style="font-family:Tahoma">PEMBANGUNAN</span></span></strong></span></span><span style="font-size:12pt"><span style="font-size:11.0pt"><span style="font-family:Arial"> menghadirkan program paket khusus, dan program-program pilihan sesuai dengan kebutuhan siswa dan Perkembangan Dunia Usaha .</span></span></span></p>
+
+<p style="text-align:justify; padding-top:10px;"><span style="font-size:12pt"><span style="font-size:11.0pt"><span style="font-family:Arial">Layanan Progam Paket :</span></span></span></p>
+
+<p><span style="font-size:16px"><span style="color:#2980b9"><em><strong>1. Paket Program Umum</strong></em></span></span></p>
+
+<ul style="padding-left: 35px">
+	<li style="text-align:justify"><span style="font-size:12pt"><span style="font-size:11.0pt"><span style="font-family:Arial">Paket Operator</span></span></span></li>
+	<li style="text-align:justify"><span style="font-size:12pt"><span style="font-size:11.0pt"><span style="font-family:Arial">Paket Internet</span></span></span></li>
+	<li style="text-align:justify"><span style="font-size:12pt"><span style="font-size:11.0pt"><span style="font-family:Arial">Desain</span></span></span></li>
+	<li style="text-align:justify"><span style="font-size:12pt"><span style="font-size:11.0pt"><span style="font-family:Arial">Teknik Komputer</span></span></span></li>
+	<li style="text-align:justify"><span style="font-size:12pt"><span style="font-size:11.0pt"><span style="font-family:Arial">Pemrograman dan Komputer Akuntansi</span></span></span></li>
+</ul>
+
+<p style="text-align:justify"><span style="font-size:16px"><span style="color:#2980b9"><em><strong><span style="font-family:Arial">2. Paket Program Unggulan</span></strong></em></span></span></p>
+
+<ul style="padding-left: 35px">
+	<li style="text-align:justify"><span style="font-size:12pt"><span style="font-size:11.0pt"><span style="font-family:Arial">Digital markrting (facebook ads, Instagram Ads &amp; Google Ads)</span></span></span></li>
+	<li style="text-align:justify"><span style="font-size:12pt"><span style="font-size:11.0pt"><span style="font-family:Arial">Internet Of Things (IOT)</span></span></span></li>
+	<li style="text-align:justify"><span style="font-size:12pt"><span style="font-size:11.0pt"><span style="font-family:Arial">Smart Home</span></span></span><span style="font-size:12pt"><span style="font-size:11.0pt"><span style="font-family:Arial">&nbsp; &nbsp; &nbsp; </span></span></span></li>
+</ul>
+
+<p style="text-align:justify"><span style="font-size:16px"><span style="color:#2980b9"><em><strong><span style="font-family:Arial">3.&nbsp;Hubungi Kami</span></strong></em></span></span></p>
+
+<div style="padding-left: 20px;">
+<table class="ml-3 table table-sm">
+	<tbody>
+		<tr>
+			<td>Alamat</td>
+			<td>:</td>
+			<td>Jl. Tentara Pelajar No.15 Pekalongan</td>
+		</tr>
+		<tr>
+			<td>Telepon</td>
+			<td>:</td>
+			<td>(0285) 412600</td>
+		</tr>
+		<tr>
+			<td>Kontak Person</td>
+			<td>:</td>
+			<td>Annang Trianggoro</td>
+		</tr>
+		<tr>
+			<td>Mobile Phone</td>
+			<td>:</td>
+			<td>0858 7850 7225</td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td>:</td>
+			<td>lpkpembangunan@yahoo.com</td>
+		</tr>
+		<tr>
+			<td>Bidang Usaha</td>
+			<td>:</td>
+			<td>Lembaga Pelatihan dan Ketrampilan</td>
+		</tr>
+	</tbody>
+</table>
+</div>
+			</div>
+            </div>
+        </div>
+    </div>
+</div>
