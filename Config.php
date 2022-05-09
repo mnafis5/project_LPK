653d826ae5004c769246ceeb04e2f6a7925839af
mnafis5 <94342046+mnafis5@users.noreply.github.com>
2022-05-09 09:07:00 +0700

    config.php

---
+++ Config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('BASEURL', 'http://localhost/project_LPK/public');
+
+define('DB_HOST','localhost');
+define('DB_USER' , 'root');
+define('DB_PASS','');
+define('DB_NAME','project_magang');
